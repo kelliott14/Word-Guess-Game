36d1336f795e44a6e6c7dadb1216d3816f36feb9
Kelliott <52241744+kelliott14@users.noreply.github.com>
2019-08-15 22:40:38 +1000

    The fridge magnet game from class gave me an idea on how to set this up. Instructions are in the js comments at the bottom of the page.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -20,6 +20,11 @@ function resetBoard(){
     guessedletters = [];
     }
 
+
+
+
+
+
 //function to set the word up as ---
 function setWord(){
     str = answerword[1];
@@ -32,10 +37,39 @@ function setWord(){
 setWord()
 resetBoard()
 
+
+
+
+//function for each keypress
+document.onkeyup = function(event){
+
+    var userGuess = event.key;
+    guessedletters.push(userGuess);
+
+    guessesleft--;   
+
+    if (userGuess = 8);{
+
+    
+    } else (guessesleft > 0){
+    guessesleft--;
+    
+}
+
 //scoreboard
-    winstally.textContent = "Wins: " + wins;
-    lossestally.textContent = "Losses: " + losses;
-    guesseslefttext.textContent = "Guesses Left: " + guessesleft;
-    guessedletterstext.textContent = "Guessed So Far: " + guessedletters;
+winstally.textContent = "Wins: " + wins;
+lossestally.textContent = "Losses: " + losses;
+guesseslefttext.textContent = "Guesses Left: " + guessesleft;
+guessedletterstext.textContent = "Guessed So Far: " + guessedletters;
+
+}
+
+//set the words up in an object with each letter as the property value
+//loop out the word to display each letter into its own div
+//set up 2 div styles: visible and ninvisible.
+//if the user guess = one of the property values, then the letter switches
+    //div style from invisible to visible
+
+
 
 
