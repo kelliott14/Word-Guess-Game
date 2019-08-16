1ee74152016a99e1eb248a69669399e5ef8548e4
Kelliott <52241744+kelliott14@users.noreply.github.com>
2019-08-16 16:44:19 +1000

    pseudo coded in comments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,7 +4,7 @@ var losses = 0;
 var guessesleft = 15;
 var guessedletters =[];
 var answerword = ["Game of Thrones","Black Mirror","The Good Place","Brooklyn Nine Nine","The Walking Dead"];
-var guessthis;
+
 
 
 var instructions = document.getElementById("instructions-text");
@@ -21,40 +21,46 @@ function resetBoard(){
     }
 
 
-
-
-
-
-//function to set the word up as ---
-function setWord(){
-    str = answerword[1];
-    guessthis = (str.replace(/[a-zA-Z]/g , '-') );
-    console.log(guessthis);
-    theanswer.textContent = guessthis;
+    //function to pull out 1 string of answerword, using a random number to select setting answerword[i]
     
-}
+    //Loop of charAt() to pull each letter from the answerword[i]
 
-setWord()
-resetBoard()
+    //put each letter into a separate div
 
+    //append this div to existing html so it appears on the screen
 
+    //set the style of the divs to an invisible style that shows only the bottom border
 
-
-//function for each keypress
-document.onkeyup = function(event){
-
-    var userGuess = event.key;
-    guessedletters.push(userGuess);
-
-    guessesleft--;   
-
-    if (userGuess = 8);{
-
+    //function for each letterpress, goes into the guessedletters var and guessesleft goes down by 1
+        //if the letterpress equals the string inside any of the divs then the div changes style to visible
+        //a correct letter then causes the correctguesses to goes up by 1
     
-    } else (guessesleft > 0){
-    guessesleft--;
+    //if the correctguesses == word.length then game is won
+        //wins goes up by 1
+        //text appears to "play again"
     
-}
+    //if the guessesleft = 0 then game is lost
+        //losses goes down by 1
+        //text appears to "play again"
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //scoreboard
 winstally.textContent = "Wins: " + wins;
@@ -62,11 +68,11 @@ lossestally.textContent = "Losses: " + losses;
 guesseslefttext.textContent = "Guesses Left: " + guessesleft;
 guessedletterstext.textContent = "Guessed So Far: " + guessedletters;
 
-}
+
 
 //set the words up in an object with each letter as the property value
 //loop out the word to display each letter into its own div
-//set up 2 div styles: visible and ninvisible.
+//set up 2 div styles: visible and invisible.
 //if the user guess = one of the property values, then the letter switches
     //div style from invisible to visible
 
